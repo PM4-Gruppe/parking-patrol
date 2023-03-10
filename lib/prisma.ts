3d91c2537a1967651fa6ba7c7609e2b1a97e50ff
Valentin Egger <eggervali@gmail.com>
2023-03-10 14:44:00 +0100

    comment to check

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,7 +7,13 @@ declare global {
 }
 
 //if (process.env.NODE_ENV === 'production') {
+  /**
+   * from: https://www.prisma.io/blog/fullstack-nextjs-graphql-prisma-2-fwpc6ds155
+   * Then if you are not in a production environment, Prisma will be attached to the global object so that you do not exhaust the database connection limit.
+   * For more details, check out the documentation for Next.js and Prisma Client best practices. (link: https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices)
+   */
   if (false) { //TODO fix
+    prisma = new PrismaClient()
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient()
