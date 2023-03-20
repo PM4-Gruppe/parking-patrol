4d7ae0c0c699eaec03be91264fc9cb5db625e47e
gabricyr <gabricyr@students.zhaw.ch>
2023-03-20 17:59:23 +0100

    fixes the return statement and did instead a if statement

--- components/molecule/PhotoChoose.tsx
+++ components/molecule/PhotoChoose.tsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 
-export const PhotoChoose: React.FC = () => {
-  const [image, setImage] = useState<string | null>(null);
-  const handleImageSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedImage = event.target.files?.[0];
-    const reader = new FileReader();
-    if (typeof selectedImage === 'undefined') return;
-    reader.readAsDataURL(selectedImage as Blob);
-    reader.onload = () => {
-      setImage(reader.result as string);
-    };
-  };
+export const PhotoChoose:
+  React.FC = () => {
+    const [image, setImage] = useState<string | null>(null);
+    const handleImageSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const selectedImage = event.target.files?.[0];
+      const reader = new FileReader();
+      if (typeof selectedImage !== 'undefined') {
+        reader.readAsDataURL(selectedImage as Blob);
+        reader.onload = () => {
+          setImage(reader.result as string);
+        };
+      }
 
-  return (
-    <div>
-      <input type="file" accept="image/*" onChange={handleImageSelect} />
-    </div>
-  );
-};
+    };
+
+    return (
+      <div>
+        <input type="file" accept="image/*" onChange={handleImageSelect} />
+      </div>
+    );
+  };
 
 export default PhotoChoose;
