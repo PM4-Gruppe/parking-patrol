e069a5300b73b6a6d8e78e952c09309c82d8502d
gabricyr <gabricyr@students.zhaw.ch>
2023-03-23 15:15:43 +0100

    changed regions to ch

--- components/atom/Informations.tsx
+++ components/atom/Informations.tsx
@@ -7,7 +7,7 @@ function getPhotoInformations(photoPath: PathLike) {
     let body = new FormData();
     body.append('upload', fs.createReadStream(photoPath));
     // Or body.append('upload', base64Image);
-    body.append('regions', 'ch-sg'); // Change to your country
+    body.append('regions', 'ch'); // Change to your country
     fetch('https://api.platerecognizer.com/v1/plate-reader/', {
         method: 'POST',
         headers: {
@@ -15,7 +15,7 @@ function getPhotoInformations(photoPath: PathLike) {
         },
         body: body,
     })
-        .then((res: Promise<Response>) => res.json())
+        .then((res: Response) => res.json())
         .then((json: JSON) => console.log(json))
         .catch((err: Error) => {
             console.log(err);
