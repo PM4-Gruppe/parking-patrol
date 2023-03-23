e4162cb530bd438864de06fa0b1a2a91aed98542
gabricyr <gabricyr@students.zhaw.ch>
2023-03-23 12:57:23 +0100

    first stage of a Request

--- components/atom/Informations.tsx
+++ components/atom/Informations.tsx
@@ -4,21 +4,24 @@ import fs, { PathLike } from 'fs'
 
 
 function getPhotoInformations(photoPath: PathLike) {
-    fs.createReadStream(photoPath)
+    let body = new FormData();
+    body.append('upload', fs.createReadStream(photoPath));
+    // Or body.append('upload', base64Image);
+    body.append('regions', 'ch-sg'); // Change to your country
     fetch('https://api.platerecognizer.com/v1/plate-reader/', {
         method: 'POST',
         headers: {
-            Authorization: "Token !mach sign up und hol dir eigene token amk!",
+            Authorization: 'Token my-token3a0effff73919f898b69ac65a32dc12347769564',
         },
         body: body,
-        })
-        .then((res) => res.json())
-        .then((json) => console.log(json))
-        .catch((err) => {
-        console.log(err);
+    })
+        .then((res: Response) => res.json())
+        .then((json: JSON) => console.log(json))
+        .catch((err: Error) => {
+            console.log(err);
         });
 }
-    
 
 
-    export default getPhotoInformations;
+
+export default getPhotoInformations;
