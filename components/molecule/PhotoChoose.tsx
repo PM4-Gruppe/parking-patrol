4822d620a88abc3776681439fec301e0bca39149
gabricyr <gabricyr@students.zhaw.ch>
2023-03-23 18:28:58 +0100

    WIP

--- components/molecule/PhotoChoose.tsx
+++ components/molecule/PhotoChoose.tsx
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
 
-export const PhotoChoose:
-  React.FC = () => {
-    const [image, setImage] = useState<string | null>(null);
-    const handleImageSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const selectedImage = event.target.files?.[0];
-      const reader = new FileReader();
-      if (typeof selectedImage !== 'undefined') {
-        reader.readAsDataURL(selectedImage as Blob);
-        reader.onload = () => {
-          setImage(reader.result as string);
-        };
-      }
-
-    };
-
-    return (
-      <div>
-        <input type="file" accept="image/*" onChange={handleImageSelect} />
-      </div>
-    );
+export const PhotoChoose: React.FC = () => {
+  const [image, setImage] = useState<string | null>(null);
+  const handleImageSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedImage = event.target.files?.[0];
+    setImage(URL.createObjectURL(selectedImage as Blob));
+    /*const reader = new FileReader();
+    if (typeof selectedImage !== 'undefined') {
+      reader.readAsDataURL(selectedImage as Blob);
+      reader.onload = () => {
+        let url = URL.createObjectURL(selectedImage);
+        setImage(reader.result as string);
+      };
+    }*/
   };
+  console.log(image);
+  return (
+    <div>
+      <input type="file" accept="image/*" onChange={handleImageSelect} />
+    </div>
+  );
+};
 
 export default PhotoChoose;
