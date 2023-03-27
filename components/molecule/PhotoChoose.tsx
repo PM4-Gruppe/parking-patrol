303fbce454837cf9a299b5b85caad4ecdc7cdd8b
Valentin Egger <eggervali@gmail.com>
2023-03-27 16:02:37 +0200

    refactoring and platerecognizer

--- components/molecule/PhotoChoose.tsx
+++ components/molecule/PhotoChoose.tsx
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react'
+import * as dotenv from 'dotenv'
 
 export const PhotoChoose: React.FC = () => {
-  const [image, setImage] = useState<string | null>(null);
-  const handleImageSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedImage = event.target.files?.[0];
-    setImage(URL.createObjectURL(selectedImage as Blob));
-    /*const reader = new FileReader();
-    if (typeof selectedImage !== 'undefined') {
-      reader.readAsDataURL(selectedImage as Blob);
-      reader.onload = () => {
-        let url = URL.createObjectURL(selectedImage);
-        setImage(reader.result as string);
-      };
-    }*/
-  };
-  console.log(image);
+  const handleImageSelect = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedImage = event.target.files?.[0]
+    const body = new FormData()
+
+    body.append('regions', 'ch'); // Change to your country
+    body.append('upload', selectedImage as Blob)
+
+    try {
+      const res = await fetch('https://api.platerecognizer.com/v1/plate-reader/', {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Token your-token-here',
+        },
+        body: body,
+      })
+      console.log(await res.json())
+      // TODO make something useful with data
+    }
+
+    catch {
+      // Fileupload not successfull
+    } 
+  }
+  
   return (
     <div>
       <input type="file" accept="image/*" onChange={handleImageSelect} />
