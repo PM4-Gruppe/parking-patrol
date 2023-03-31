e875c3f82201afd8e0e27d3738ee45dfcc056454
Philemon Wildberger <wilphi01@students.zhaw.ch>
2023-03-31 11:13:09 +0200

    npp-68: fix migration.sql file error

--- prisma/migrations/20230331084754_/migration.sql
+++ prisma/migrations/20230331084754_/migration.sql
@@ -4,8 +4,6 @@
   - The primary key for the `ParkedCar` table will be changed. If it partially fails, the table could be left without primary key constraint.
 
 */
--- DropIndex
-DROP INDEX "ParkedCar_numberPlate_key";
 
 -- AlterTable
 ALTER TABLE "ParkedCar" DROP CONSTRAINT "ParkedCar_pkey",
