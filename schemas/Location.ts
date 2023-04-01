04b3dc499d36c65c6f9090345eb1f1d8767a2e7e
gabricyr <gabricyr@students.zhaw.ch>
2023-04-01 21:11:42 +0200

    added missed interfaces -> still not wokring

---
+++ schemas/Location.ts
@@ -0,0 +1,5 @@
+//TODO - add properties to Location interface
+export interface Location {
+    latitude: number[],
+    longitude: number[]
+}
---
+++ schemas/PhotoInformation.ts
@@ -0,0 +1,7 @@
+import { Location } from './Location';
+import { LicensePlate } from './LicensePlate';
+
+export interface PhotoInformation {
+    licensePlate: LicensePlate,
+    location: Location
+}
