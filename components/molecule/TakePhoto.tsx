cbfc36283df935a0949e369a7e3b8ba08bdfcd73
van <quachvan@students.zhaw.ch>
2023-04-02 20:02:05 +0200

    take a photo, download it and store it in blop

--- components/molecule/TakePhoto.tsx
+++ components/molecule/TakePhoto.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const TakePhoto: React.FC = () => {
     const [stream, setStream] = useState<MediaStream | null>(null);
+    const [videoRef, setVideoRef] = useState<HTMLVideoElement | null>(null);
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [photoPath, setPhotoPath] = useState<string | null>(null);
 
     const handleStartClick = async () => {
         try {
@@ -18,6 +21,40 @@ const TakePhoto: React.FC = () => {
                 track.stop();
             });
             setStream(null);
+            setPhotoPath(null);
+            setVideoRef(null);
+        }
+    };
+
+    const handleTakePhotoClick = () => {
+        if (videoRef && canvasRef.current) {
+            const video = videoRef;
+            const canvas = canvasRef.current;
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            const context = canvas.getContext('2d');
+            if (context) {
+                context.drawImage(video, 0, 0);
+                canvas.toBlob((blob) => {
+                    if (blob !== null) {
+                        const dataUrl = URL.createObjectURL(blob);
+                        const a = document.createElement('a');
+                        a.href = dataUrl;
+                        a.download = 'photo.png';
+                        document.body.appendChild(a);
+                        a.click();
+                        document.body.removeChild(a);
+                        setPhotoPath(a.href);
+                    }
+                }, 'image/png');
+            }
+        }
+    };
+
+    const handleVideoRef = (video: HTMLVideoElement | null) => {
+        if (video) {
+            video.srcObject = stream;
+            setVideoRef(video);
         }
     };
 
@@ -26,8 +63,11 @@ const TakePhoto: React.FC = () => {
             <h1>Camera Example</h1>
             {stream ? (
                 <>
-                    <video autoPlay ref={(video) => video && (video.srcObject = stream)} />
+                    <video autoPlay ref={handleVideoRef} />
+                    <canvas style={{ display: 'none' }} ref={canvasRef} />
+                    <button onClick={handleTakePhotoClick}>Take Photo</button>
                     <button onClick={handleStopClick}>Stop</button>
+                    {photoPath && <div>{photoPath}</div>}
                 </>
             ) : (
                 <button onClick={handleStartClick}>Start</button>
