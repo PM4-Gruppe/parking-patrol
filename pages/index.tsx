99df319cf01d6a0a76cb27db5a1bf074126ab5dd
Philemon Wildberger <wilphi01@students.zhaw.ch>
2023-04-04 17:46:35 +0200

    NPP-11: removed hard coded entry

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { parkedCars } from '../data/parkedCars'
 import { gql, useQuery } from '@apollo/client'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 
@@ -31,22 +30,6 @@ export default function Home() {
           {data.parkedCars.map((plate: any) => (
             <li key={plate.sign}>
               {plate.numberPlate}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-
-      <div className="container mx-auto max-w-5xl my-20">
-        <p className="text-3xl">Hardcoded content:</p>
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {parkedCars.map((car) => (
-            <li key={'missing key'} className="shadow  max-w-md  rounded">
-              <img className="shadow-sm" src={String(car.photoPath)} />
-              <div className="p-5 flex flex-col space-y-2">
-                <p className="text-lg font-medium">Titel</p>
-                <p className="text-gray-600">{car.numberPlate}</p>
-              </div>
             </li>
           ))}
         </ul>
