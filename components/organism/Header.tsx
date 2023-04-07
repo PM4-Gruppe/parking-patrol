a9355a7ad46467885db7c27b63095d5a98a0861c
Lukas Zimmermann <lukas@air-von-lukas.zhaw.ch>
2023-04-07 15:51:19 +0200

    style header

--- components/organism/Header.tsx
+++ components/organism/Header.tsx
@@ -6,25 +6,23 @@ const Header = () => {
   const { user } = useUser()
 
   return (
-    <header>
-      <h1 className="text-4xl text-rose-600">HEADER</h1>
-      Link zu Github Repo:{''}
-      <Link href="https://github.com/PM4-Gruppe/parking-patrol">hier</Link>{' '}
+    <div className='bg-neutral-800 p-5'>
       <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          {user ? (
-            <div className="flex items-center space-x-5">
-              <Link href="/api/auth/logout" className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-                Logout
-              </Link>
-              <img alt="profile" className="rounded-full w-12 h-12" src={user.picture ? user.picture : ''} />
-            </div>
-          ) : (
-            <Link href="/api/auth/login" className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-              Login
+        {user ? (
+          <div className="flex items-center space-x-10">
+            <Link href="/api/auth/logout" className='absolute left-5 rounded-full items-center bg-neutral-600 hover:bg-neutral-700 p-3 w-12 h-12 cursor-pointer'>
+              <img src='logout_icon_48dp.png' />
             </Link>
-          )}
-        </nav>
-    </header>
+            <img alt='PolizeiLogo' className='w-32' src='/SH_Polizei_Logo.png' />
+            <img alt="profile" className="absolute right-5 rounded-full w-12 h-12" src={user.picture ? user.picture : ''} />
+          </div>
+        ) : (
+          <Link href="/api/auth/login" className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Login
+          </Link>
+        )}
+      </nav>
+    </div>
   )
 }
 
