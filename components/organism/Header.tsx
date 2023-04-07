386692058e40cff8384cd06f5ab05e77f6bba62e
Lukas Zimmermann <lukas@air-von-lukas.zhaw.ch>
2023-04-07 15:58:19 +0200

    remove singlequote

--- components/organism/Header.tsx
+++ components/organism/Header.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
 import { useUser } from '@auth0/nextjs-auth0/client'
+import Image from 'next/image'
 
 const Header = () => {
   const { user } = useUser()
 
   return (
-    <div className='bg-neutral-800 p-5'>
+    <div className="bg-neutral-800 p-5">
       <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
         {user ? (
           <div className="flex items-center space-x-10">
-            <Link href="/api/auth/logout" className='absolute left-5 rounded-full items-center bg-neutral-600 hover:bg-neutral-700 p-3 w-12 h-12 cursor-pointer'>
-              <img src='logout_icon_48dp.png' />
+            <Link href="/api/auth/logout" className="absolute left-5 rounded-full items-center bg-neutral-600 hover:bg-neutral-700 p-3 w-12 h-12 cursor-pointer">
+              <Image alt="logoutIcon" src="/logout_icon_48dp.png" width={50} height={50} />
             </Link>
-            <img alt='PolizeiLogo' className='w-32' src='/SH_Polizei_Logo.png' />
-            <img alt="profile" className="absolute right-5 rounded-full w-12 h-12" src={user.picture ? user.picture : ''} />
+            <Image alt="PolizeiLogo" className="w-32" src="/SH_Polizei_Logo.png" width={300} height={300} />
+            <img alt="profile" className="absolute right-5 rounded-full w-12 h-12" width={50} height={50} src={user.picture ? user.picture : ''} />
           </div>
         ) : (
           <Link href="/api/auth/login" className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
