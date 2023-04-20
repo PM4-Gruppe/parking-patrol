0b38c22503317a1b443898e0b65c33edba43b757
Lukas Zimmermann <lukas@air-von-lukas.zhaw.ch>
2023-04-20 12:36:33 +0200

    create index page

---
+++ src/components/molecule/Widget.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+interface MyComponentProps {
+    title: string;
+    content: string;
+}
+
+export const Widget = ({ title, content }: MyComponentProps) => {
+    let fontStyle: string = 'text-white font-light'
+    if (content.length < 5) {
+        fontStyle += ' text-5xl text-center p-2'
+    } else {
+        fontStyle += ' text-2xl p-4'
+    }
+
+    return (
+        <div className="bg-neutral-500/50 rounded-2xl">
+            <div className="bg-green-700 p-3 text-xl text-white rounded-t-2xl">
+                {title}
+            </div>
+            <p className={fontStyle}>
+                {content}
+            </p>
+        </div>
+    );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,46 @@
 import Head from 'next/head'
 import { gql, useQuery } from '@apollo/client'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
-
-const AllVehiclesQuery = gql(`
-  query {
-    parkedCars {
-      numberPlate,
-      photoPath
-    }
-  }
-`)
-
+import Link from 'next/link'
+import Image from 'next/image'
+import { Widget } from '../components/molecule/Widget'
+import { BiCamera } from 'react-icons/bi'
+import { FiSettings } from 'react-icons/fi'
+import { FaListUl } from 'react-icons/fa'
 
 export default function Home() {
-  const { data, loading, error } = useQuery(AllVehiclesQuery)
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Oh no... {error.message}</p>
-
   return (
-    <div>
+    <div className="mb-auto">
       <Head>
-        <title>Parking Patrol</title>
+        <title>Parking Patrol</title>it
         <link rel="icon" href="/favicon.svg" />
       </Head>
-
-      <div className="container mx-auto max-w-5xl my-20">
-        <p className="text-3xl">Kontrollierte Fahrzeuge:</p>
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {data.parkedCars.map((plate: any) => (
-            <li key={plate.numberPlate}>
-              <img className="shadow-sm" src={String(plate.photoPath)} />
-              <div className="p-5 flex flex-col space-y-2">
-                <p className="text-gray-600">{plate.numberPlate}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
+      <div className="flex flex-col center justify-evenly m-10">
+        <div className="p-5">
+          <Widget title={"Aktuelle Parkzone"} content={"Beispielstrasse"} />
+        </div>
+        <div className="flex flex-row center justify-between">
+          <div className='w-1/2 p-5'>
+            <Widget title={"Erfasste Fahrzeuge"} content={"-"} />
+          </div>
+          <div className="w-1/2 p-5">
+            <Widget title={"Offene Vergehen"} content={"-"} />
+          </div>
+        </div>
+      </div>
+      <div id="main" className="flex justify-center w-100 h-full">
+        <div className="absolute inset-x-0 bottom-0 h-28 bg-neutral-900" />
+        <Link className="absolute bottom-16" href="/camera">
+          <div className="bg-green-700 hover:bg-green-800 rounded-full p-5 border-solid border-8 border-neutral-800">
+            <BiCamera className='w-20 h-20 p-1 text-white' />
+          </div>
+        </Link>
+        <Link className="absolute bottom-5 left-20 rounded-full items-center bg-neutral-900 hover:bg-neutral-700" href={'https://en.wikipedia.org/wiki/Settings_(Windows)'}>
+          <FiSettings className='w-20 h-20 p-4 text-white' />
+        </Link>
+        <Link className="absolute bottom-6 right-20 rounded-full items-center bg-neutral-900 hover:bg-neutral-700" href={'https://en.wikipedia.org/wiki/List_(information)'}>
+          <FaListUl className='w-20 h-20 p-4 text-white' />
+        </Link>
       </div>
     </div>
   )
