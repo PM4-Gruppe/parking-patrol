8ec3b7d077f454851bf6e93f4be0cb7c24bad4d2
jasarard <jasarard@students.zhaw.ch>
2023-04-22 18:28:13 +0200

    NPP-85: protect api route and camera page

--- src/pages/camera.tsx
+++ src/pages/camera.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import { PhotoChoose } from '../components/molecule/PhotoChoose'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 
 export default function Camera() {
-  
+
     return (
         <div className="flex flex-col bg-gradient-to-t from-neutral-800 to-neutral-700 min-h-screen">
             <Head>
@@ -13,3 +14,5 @@ export default function Camera() {
         </div>
     )
 }
+
+export const getServerSideProps = withPageAuthRequired();
