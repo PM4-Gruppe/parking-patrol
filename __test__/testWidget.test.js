ea401d6aba4e4b93bf8d43a82a7e4019cd8fd5d4
thofun <thofun@outlook.com>
2023-05-01 23:39:17 +0200

    npp-103: testing if Widget renders

---
+++ __test__/testWidget.test.js
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import {render} from '@testing-library/react';
+import { Widget } from '../src/components/molecule/Widget';
+import '@testing-library/jest-dom'
+
+describe('Widget', () => {
+    test('renders with title and children', () => {
+        const title = 'My Widget';
+        const content = <div>Widget content</div>;
+        const { getByText } = render(<Widget title={title}>{content}</Widget>);
+        const titleElement = getByText(title);
+        const contentElement = getByText('Widget content');
+        expect(titleElement).toBeInTheDocument();
+        expect(contentElement).toBeInTheDocument();
+    });
+});
