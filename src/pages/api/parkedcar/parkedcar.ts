2834b5ed886f9a9435fddeb2011f22a44d1a8018
Philemon Wildberger <wilphi01@students.zhaw.ch>
2023-05-01 14:39:30 +0200

    NPP-106: add findCarModel

--- src/pages/api/parkedcar/parkedcar.ts
+++ src/pages/api/parkedcar/parkedcar.ts
@@ -4,8 +4,8 @@ export default async function handler(req: any, res: any) {
     if (req.method === 'POST') {
         // Process a POST request
         try {
-            const { numberPlate, controlTime, modelName, colorName, latitude, longitude, carInspector, photoPath } = req.body;
-            const newParkedCar = await createParkedCar(numberPlate, controlTime, modelName, colorName, latitude, longitude, carInspector, photoPath);
+            const { numberPlate, controlTime, modelName, manufacturer, colorName, latitude, longitude, carInspector, photoPath } = req.body;
+            const newParkedCar = await createParkedCar(numberPlate, controlTime, modelName, manufacturer, colorName, latitude, longitude, carInspector, photoPath);
             res.status(201).json(newParkedCar);
         } catch (error: any) {
             res.status(500).json({ message: error.message });
--- src/utils/createParkedCar.js
+++ src/utils/createParkedCar.js
@@ -2,12 +2,15 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export async function createParkedCar(numberPlate, controlTime, modelName, colorName, latitude, longitude, carInspector, photoPath) {
+export async function createParkedCar(numberPlate, controlTime, modelName, manufacturer, colorName, latitude, longitude, carInspector, photoPath) {
+  console.log('Before findCarModel')
+  const carModel = await findCarModel(modelName, manufacturer);
+  console.log('Before parkedCar')
   const newParkedCar = await prisma.parkedCar.create({
     data: {
       numberPlate,
       controlTime,
-      carModel: { connect: { modelName: modelName } },
+      carModel: { connect: { modelName: carModel.modelName, manufacturer: carModel.manufacturer } },
       carColor: { connect: { colorName: colorName } },
       latitude,
       longitude,
@@ -19,5 +22,26 @@ export async function createParkedCar(numberPlate, controlTime, modelName, color
       carColor: true
     }
   })
+  console.log(`Created parked car with number plate ${newParkedCar.numberPlate}`)
   return newParkedCar;
+}
+
+async function findCarModel(modelName, manufacturer) {
+  console.log(`Searching for car model ${modelName} from ${manufacturer}`)
+
+  try {
+  const searchModel = await prisma.carModel.findFirst({
+    where: {
+      modelName: modelName,
+      manufacturer: manufacturer
+    }
+  })
+
+  console.log(`Car model ${searchModel.modelName} from ${searchModel.manufacturer} found`)
+
+  return searchModel;
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Error searching for car model ${modelName} from ${manufacturer}`)
+  }
 }
