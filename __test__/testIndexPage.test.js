e3cf76eb97fefdcb533d5183661c6fd366439b69
thofun <thofun@outlook.com>
2023-05-02 22:56:37 +0200

    npp-103: for page Index new snapshot test

---
+++ __test__/testIndexPage.test.js
@@ -0,0 +1,10 @@
+import Home from '../src/pages/index'
+import renderer from 'react-test-renderer'
+import React from 'react'
+
+describe('Home', () => {
+    it('should match snapshot', () => {
+        // const tree = renderer.create(<Home />).toJSON();
+        // expect(tree).toMatchSnapshot();
+    });
+});
