a131a07703d3d24dc33dc1d47629d0b1811206c6
Philemon Wildberger <wilphi01@students.zhaw.ch>
2023-05-10 00:36:19 +0200

    NPP-114: adjustment of the API commented out

--- src/pages/api/parkedcar/parkedcar.ts
+++ src/pages/api/parkedcar/parkedcar.ts
@@ -1,12 +1,28 @@
-import { createParkedCar } from '../../../lib/createParkedCar';
+import { CREATE_PARKED_CAR } from '../../../graphql/mutations/parkedCar.create';
+import { useMutation } from '@apollo/client'
 
 export default async function handler(req: any, res: any) {
     if (req.method === 'POST') {
         // Process a POST request
         try {
             const { numberPlate, controlTime, modelName, manufacturer, colorName, latitude, longitude, carInspector, photoPath } = req.body;
-            const newParkedCar = await createParkedCar(numberPlate, controlTime, modelName, manufacturer, colorName, latitude, longitude, carInspector, photoPath);
-            res.status(201).json(newParkedCar);
+
+            /*const [createParkedCarVariable, { data, loading, error }] = useMutation(CREATE_PARKED_CAR);
+
+            createParkedCarVariable({
+                variables: {
+                    numberPlate: numberPlate,
+                    carModel: modelName,
+                    carManufacturer: manufacturer,
+                    carColor: colorName,
+                    latitude: latitude,
+                    longitude: longitude,
+                    carInspector: carInspector,
+                    photoPath: photoPath,
+                },
+            });*/
+
+            res.status(201).json('data');
         } catch (error: any) {
             res.status(500).json({ message: error.message });
         }
