f9e009f4770ffb2b234e1907215eb60055912cb0
thofun <thofun@outlook.com>
2023-05-10 23:36:01 +0200

    npp-103: added new tests

--- __test__/testimage-upload.test.js
+++ __test__/testimage-upload.test.js
@@ -5,9 +5,33 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import handler from '../src/pages/api/image-storage/image-upload';
+import { NextApiHandler } from 'next';
 
 describe('image-upload', () => {
     it('calls handler function', async () => {
+
+    });
+
+    it('returns a 500 status code', async () => {
+        const req = {
+            method: 'POST',
+            body: {},
+            files: {
+                image: {
+                    path: 'test-image.jpg',
+                    name: 'test-image.jpg',
+                },
+            },
+        };
+        const res = {
+            status: jest.fn(() => res),
+            json: jest.fn(),
+        };
+        await handler(req, res);
+        expect(res.status).toHaveBeenCalledWith(500);
+    });
+
+    it('returns a 200 status code', async () => {
 
     });
 });
