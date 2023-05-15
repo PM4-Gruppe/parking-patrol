f2dc4fda9ce4818a92b6a1dd57895a1ed79eb91f
Philemon Wildberger <wilphi01@students.zhaw.ch>
2023-05-15 08:05:59 +0200

    NPP-114: add CarManufacturer query

--- src/graphql/types/CarManufacturer.ts
+++ src/graphql/types/CarManufacturer.ts
@@ -1,3 +1,4 @@
+import { prisma } from '../../lib/prisma'
 import { builder } from '../builder'
 
 builder.prismaObject('CarManufacturer', {
@@ -5,3 +6,11 @@ builder.prismaObject('CarManufacturer', {
         manufacturerName: t.exposeID('manufacturerName'),
     })
 })
+
+builder.queryField('carManufacturers', (t) =>
+    t.prismaField({
+        type: ['CarManufacturer'],
+        resolve: (query, _parent, _args, _ctx, _info) =>
+            prisma.carManufacturer.findMany({ ...query }),
+    })
+)
