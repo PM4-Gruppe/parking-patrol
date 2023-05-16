59a0997a777db4c0a93ce086fe7927a974a6009e
Valentin Egger <eggervali@gmail.com>
2023-05-16 14:25:49 +0200

    npp-114: solve merge conflict

--- src/graphql/types/CarManufacturer.ts
+++ src/graphql/types/CarManufacturer.ts
@@ -3,23 +3,15 @@ import { builder } from '../builder'
 import { prisma } from '../../lib/prisma'
 
 builder.prismaObject('CarManufacturer', {
-    fields: (t) => ({
-        manufacturerName: t.exposeID('manufacturerName'),
-    })
+  fields: (t) => ({
+    manufacturerName: t.exposeID('manufacturerName'),
+  }),
 })
 
 builder.queryField('carManufacturers', (t) =>
-<<<<<<< HEAD
   t.prismaField({
     type: ['CarManufacturer'],
     resolve: (query, _parent, _args, _ctx, _info) =>
       prisma.carManufacturer.findMany({ ...query }),
   })
-=======
-    t.prismaField({
-        type: ['CarManufacturer'],
-        resolve: (query, _parent, _args, _ctx, _info) =>
-            prisma.carManufacturer.findMany({ ...query }),
-    })
->>>>>>> dev
 )
