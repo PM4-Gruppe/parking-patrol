76d38a06b6e739f5f3c671ac71e9a8bff48c5226
Lukas Zimmermann <lukas@MacBook-Air-von-Lukas.local>
2023-06-23 17:46:45 +0200

    fix layout

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="max-h-screen bg-gradient-to-t from-neutral-900 to-neutral-700">
+    <div className="h-screen bg-gradient-to-t from-neutral-900 to-neutral-700">
       <Header />
       {children}
     </div>
