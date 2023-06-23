711c096d45fcd2f9970d1df6c3cd3f25067891af
rosinluc <rosinluc@students.zhaw.ch>
2023-06-23 16:48:50 +0200

    add page with car photo

---
+++ src/pages/cardetails.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {useRouter} from "next/router";
+import {withPageAuthRequired} from '@auth0/nextjs-auth0'
+import {ParkedCar} from "@prisma/client";
+import {isQualifiedName} from "tsutils";
+import Image from "next/image";
+import Head from "next/head";
+import {Button} from "../components/atom/Button";
+
+
+
+// @ts-ignore
+const Cardetails = () => {
+
+    const handleClickGoToIndex = () => {
+        router.push('/carlist')
+    }
+
+
+    const router = useRouter()
+    const {carpath} = router.query
+    return (
+        <div className=''>
+            <Head>
+                <title>Parking Patrol</title>
+                <link rel="icon" href="/favicon.svg"/>
+            </Head>
+            <p className='m-5 text-white text-3xl'>Car Details</p>
+            <div className='flex flex-col'>
+                <div className='m-5 p-1 grow rounded-lg bg-neutral-700 overflow-auto drop-shadow-xl'>
+                    <Image
+                        src={String(carpath)}
+                        width={500}
+                        height={500}
+                        alt="Picture of the author"
+                    />
+                </div>
+            </div>
+            <div className='m-5'>
+                <Button label='Zurück' onClick={handleClickGoToIndex}/>
+            </div>
+        </div>
+    );
+};
+
+export default Cardetails;
+export const getServerSideProps = withPageAuthRequired();
