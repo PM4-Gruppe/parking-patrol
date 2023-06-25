3ca2e79f930b759fb6173b4f0313561f5b2607dd
thofun <thofun@outlook.com>
2023-06-25 17:14:13 +0200

    NPP-115: test for imageProvider

---
+++ __test__/testImageProvider.test.js
@@ -0,0 +1,30 @@
+import { uploadImage, downloadImage } from '../src/lib/imageProvider';
+
+jest.mock('@aws-sdk/client-s3');
+
+describe('uploadImage', () => {
+    it('should call the uploadImage function', async () => {
+        const mockSend = jest.fn().mockResolvedValue({}); // Mock the S3 send function
+        S3.prototype.send = mockSend;
+
+        const filename = 'example.jpg';
+        const image = Buffer.from('image content');
+
+        await uploadImage(filename, image);
+
+        expect(mockSend).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('downloadImage', () => {
+    it('should call the downloadImage function', async () => {
+        const mockSend = jest.fn().mockResolvedValue({ Body: Buffer.from('image content') }); // Mock the S3 send function
+        S3.prototype.send = mockSend;
+
+        const filename = 'example.jpg';
+
+        await downloadImage(filename);
+
+        expect(mockSend).toHaveBeenCalledTimes(1);
+    });
+});
